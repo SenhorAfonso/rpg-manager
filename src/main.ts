24973bdd80bbc7ce13acb9c22e5d75a3c5269b5a
Pedro Afonso <sirafonso005896@outlook.com>
2024-06-14 15:48:02 -0300

    set global interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import AppModule from './app.module';
+import LogginInterceptor from './common/interceptor/LoggerInterceptor';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LogginInterceptor())
   await app.listen(3000);
 };
 
